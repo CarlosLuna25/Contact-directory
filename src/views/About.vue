a96868874fbdb46794adb20d903f079e7ac3e20f
Carlos <cl72506@gmail.com>
2020-11-12 21:22:26 -0400

    ruta de agregar contacto funcionando sin validaciones

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="hero is-danger is-bold is-gradient">
+       <b-image
+    
+            src="https://picsum.photos/100/100"
+            alt="A random image"
+           
+            srcset="https://picsum.photos/id/1062/400/225 200w, https://picsum.photos/id/1062/800/450  200w, https://picsum.photos/id/1062/1600/900 300w"
+
+        >
+      
+
+        </b-image>
+          <div class="hero-body">
+          <div class="columns is-centered">
+              <h1 class="title has-text-black">About Contact directory app</h1>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
--- src/views/contact/add.vue
+++ src/views/contact/add.vue
@@ -1,5 +1,66 @@
 <template>
     <div>
-        Añadir nuevo Contacto
+      
+        <div class="card">
+            <label for="nombre" > Nombre de contacto </label>
+            <input type="text" v-model="name" name="nombre" id="name">
+            <label for="nombre" > email de contacto </label>
+            <input type="text" v-model="email" name="nombre" id="name">
+            <br>
+             <label for="nombre" > Telefono</label>
+            <input type="text" v-model="phone" name="nombre" id="name">
+             <label for="nombre" > Direccion </label>
+            <input type="text" v-model="direction" name="direction" id="name">
+            <br>
+
+            <button v-if="isFull" class="button is-rounded is-success " @click="Agregar()">Agregar</button>
+        </div>
     </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+    
+    data(){
+        return {
+            informacion:true  ,
+            name:"",
+            email:"",
+            phone:"",
+            direction:"",
+        }
+    }, 
+    computed:{
+           isFull(){
+            if(this.name.length>=3){
+                return true
+            }else{
+                return false
+            }
+        },
+    },
+    methods:{
+     
+       async Agregar(){
+           await axios.post(
+               process.env.VUE_APP_server + "add",{
+                   name:this.name,
+                   email:this.email,
+                   phone:this.phone,
+                   direction:this.direction
+               },
+               {
+          headers: {
+            Authorization: process.env.VUE_APP_authorization,
+          }}
+           ).then((response) => {
+          const data = response;
+          console.log(data)
+          }).catch((e) => {
+          console.log(e);
+        })
+        }
+    }
+}
+</script>
